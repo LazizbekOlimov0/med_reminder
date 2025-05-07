eacd0db432df68f19ffea5a9cfbef920af63a949
STaikk0 <kalibrealpha88@gmail.com>
2025-05-07 09:02:43 +0500

    fixed api_service.dart , auth_controller.dart

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,18 +1,10 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import '../services/api_service.dart';
 
 class AuthController {
   Future<bool> login(BuildContext context, String email, String password) async {
     try {
-      final response = await http.post(
-        Uri.parse('https://10.10.1.9:7190/api/Users/login'),
-        body: {
-          'email': email,
-          'password': password,
-        },
-      );
+      final response = await ApiService.login(email, password);
 
       if (response.statusCode == 200) {
         return true;
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,15 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService{
-  static const String baseUrl = "https://localhost:7190/api/Users/login";
+  static const String baseUrl = "https://10.10.1.9:7190/api/Users/login";
 
   static Future<http.Response> login(String email, String password){
     final url = Uri.parse('$baseUrl/login');
     return http.post(
       url,
-      headers: {"Content type": "application/json"},
-      body: '{"email": "$email", "password": "$password"}',
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": email, "password": password}),
     );
   }
 }
