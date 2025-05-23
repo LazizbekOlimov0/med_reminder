4dcd15237c44c0cbc6135efd948987434a3a4cb8
STaikk0 <kalibrealpha88@gmail.com>
2025-05-23 11:31:32 +0500

    fix api_service.dart fila

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService{
-  static const String baseUrl = "http://46.101.186.244:7150/api/Users/login";
+  static const String baseUrl = "http://46.101.186.244:7150";
 
   static Future<http.Response> login(String email, String password){
-    final url = Uri.parse('$baseUrl/login');
+    final url = Uri.parse('$baseUrl/api/Users/login');
     return http.post(
       url,
       headers: {"Content-Type": "application/json"},
